0f8bc6986b21e873509f1327f4f032ee6f120ff6
Parvez Ali <parvezsdet@gmail.com>
2022-06-29 23:11:30 +0530

    Cyrax has done the commit

--- src/main/java/PracticeProgram/AbstractInherit.java
+++ src/main/java/PracticeProgram/AbstractInherit.java
@@ -4,6 +4,7 @@ public class AbstractInherit extends AbstractInstance{
     @Override
     public void test1() {
         System.out.println("Rain");
+        System.out.println("Rain");
     }
     public static void main(String arg[]){
        // AbstractInstance ab = new AbstractInstance();
