435052a5203825ce92800238fc26ced26b371c45
Parvez Ali <parvezsdet@gmail.com>
2022-06-30 00:09:35 +0530

    New Changes from IntelliJ

--- src/main/java/PracticeProgram/TestVowel.java
+++ src/main/java/PracticeProgram/TestVowel.java
@@ -4,5 +4,6 @@ public class TestVowel {
     public static void main(String [] at){
        // VowelProgram s= new VowelProgram();
         System.out.println(new VowelProgram().vowelCount("Jax"));
+        System.out.println(new VowelProgram().vowelCount("Jax"));
     }
 }
